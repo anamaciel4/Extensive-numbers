dfaf86f3e4a3af3d405fb10b7d4c429c913c5c10
Ana Caroline <129456378+anamaciel4@users.noreply.github.com>
2023-05-27 17:23:08 -0300

    Add files via upload
    Update in Project Python

--- index.py
+++ index.py
@@ -1,4 +1,7 @@
-print ("Seja bem-vindo(a)!")
+import time
+import os
+
+print ("{:^73}".format("\033[33m\033[1mSeja bem-vindo(a)!\033[0;0m"))
 
 def binary_subtraction(a, b):
     return bin(int(a, 2) - int(b, 2))[2:]
@@ -6,65 +9,138 @@ def binary_addition(a, b):
     return bin(int(a, 2) + int(b, 2))[2:]
 opcao = -5
 
+#Início
 while True:
-  print ('------------------------------------------------------------------')
+  print ('\033[34m------------------------------------------------------------------\033[0;0m')
   print ("""Escolha um cálculo:
-[1] Conversão da base decimal para as bases binário e octadecimal
+[1] Conversão da base decimal para as bases binário e octadecimal.
 [2] Conversão das bases binário e octadecimal para decimal.
-[3] Calculadora aritmética de binários (Soma e subtração)
+[3] Calculadora aritmética de binários (Soma e subtração).
 [4] Encerrar""")
-  print ('------------------------------------------------------------------')
+  print ('\033[34m------------------------------------------------------------------\033[0;0m')
   opcao=int(input("Digite sua opção: "))
-  print ('------------------------------------------------------------------')
+  print ('\033[34m------------------------------------------------------------------\033[0;0m')
+  print('')
 
+  #Primeira opção
   if opcao == 1:
     num=(int(input("Digite um número da base decimal: ")))
-    print ("{} convertido para binário é igual {:b}".format(num, (num)))
-  
+    print (f"{num} convertido para binário é igual", "\033[32m\033[1m{:b}\033[0;0m".format(num, (num)))
   if opcao == 1:
-    print ("{} convertido em hexadecimal é igual {:o}".format(num, (num)))
+    print (f"{num} convertido para octadecimal é igual", "\033[32m\033[1m{:o}\033[0;0m".format(num, (num)))
+    print ('')
  
   if opcao == 2:
     nb=(input("Digite um número binário: "))
     try:
       b=int(nb,2)
-      print (f"{nb} convertido para decimal é igual {b}")
+      print (f"{nb} convertido para decimal é igual \033[32m\033[1m{b}\033[0;0m")
+      print ('')
     except:
-      print("O número digitado não é binário!")
-
-      
-    print ('------------------------------------------------------------------')
+      print("\033[31m\033[1mO número digitado não é binário!\033[0;0m")
+      print('')
+    print ('\033[34m------------------------------------------------------------------\033[0;0m')
+    print('')
+    
+  #Segunda opção
   if opcao == 2:
-    nb=(input("Digite um número octal: "))
-    o=int(nb,8)
-    print (f"{nb} convertido para decimal é igual {o}")
+    try:
+      nb=(input("Digite um número octal: "))
+      o=int(nb,8)
+      print (f"{nb} convertido para decimal é igual \033[32m\033[1m{o}\033[0;0m")
+      print('')
+    except:
+      print("\033[31m\033[1mNúmero inválido, pois a base octaldecimal comporta apenas de 0 a 7.\033[0;0m")
+      print ('')
 
+  #Terceira opção
   elif opcao == 3:
+    print ('\033[34m------------------------------------------------------------------\033[0;0m')
     print("""Escolha opção desejada: 
   [1] Subtração
   [2] Soma """)
-    print ('------------------------------------------------------------------')
+    print ('\033[34m------------------------------------------------------------------\033[0;0m')
     opccao = int(input("Digite sua opção: "))
-    print ('------------------------------------------------------------------')
+    print ('\033[34m------------------------------------------------------------------\033[0;0m')
+    print ('')
     if opccao == 1:
       numero_binario = input('Digite o primeiro número binário: ')
       numero_binario2 = input('Digite o segundo número binário: ')
       try:
         resultado_subtração = binary_subtraction(numero_binario, numero_binario2)
-        print(' Resultado da subtração: ', resultado_subtração)
+        print(f'Resultado da subtração: \033[32m\033[1m{resultado_subtração}\033[0;0m')
+        print ('')
       except:
-        print("Os números digitados não são binários!")
+        print("\033[31m\033[1mOs números digitados não são binários!\033[0;0m")
+        print ('')
             
     if opccao == 2:
       numero_binario1 = input('Digite o primeiro número binário: ')
       numero_binario3 = input('Digite o segundo número binário: ')
       try:
         resultado_soma = binary_addition(numero_binario1, numero_binario3)
-        print('Resultado da soma: ', resultado_soma)
+        print(f'Resultado da soma: \033[32m\033[1m{resultado_soma}\033[0;0m')
+        print ('')
       except:
-          print("Os números digitados não são binários!")
-      
+          print("\033[31m\033[1mOs números digitados não são binários!\033[31m")
+          print ('')
 
+  #Encerramento
   elif opcao == 4:
-    print('Encerrando o programa...')
+    print("F")
+    time.sleep(0.2)
+    os.system('clear')
+    print("Fe")
+    time.sleep(0.2)
+    os.system('clear')
+    print("Fec")
+    time.sleep(0.2)
+    os.system('clear')
+    print("Fech")
+    time.sleep(0.2)
+    os.system('clear')
+    print("Fecha")
+    time.sleep(0.2)
+    os.system('clear')
+    print("Fechan")
+    time.sleep(0.2)
+    os.system('clear')
+    print("Fechand")
+    time.sleep(0.2)
+    os.system('clear')
+    print("Fechando")
+    time.sleep(0.2)
+    os.system('clear')
+    print("Fechando o")
+    time.sleep(0.2)
+    os.system('clear')
+    print("Fechando o pr")
+    time.sleep(0.2)
+    os.system('clear')
+    print("Fechando o pro")
+    time.sleep(0.2)
+    os.system('clear')
+    print("Fechando o prog")
+    time.sleep(0.2)
+    os.system('clear')
+    print("Fechando o progr")
+    time.sleep(0.2)
+    os.system('clear')
+    print("Fechando o progra")
+    time.sleep(0.2)
+    os.system('clear')
+    print("Fechando o program")
+    time.sleep(0.2)
+    os.system('clear')
+    print("Fechando o programa")
+    time.sleep(0.2)
+    os.system('clear')
+    print("Fechando o programa.")
+    time.sleep(0.2)
+    os.system('clear')
+    print("Fechando o programa..")
+    time.sleep(0.2)
+    os.system('clear')
+    print("Fechando o programa...")
+
     break
